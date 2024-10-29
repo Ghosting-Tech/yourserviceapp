a397c99dfab04b34de6cee2b8b744c5b24a70038
Mahtab6kn <mahtab6kn@gmail.com>
2024-10-29 14:37:01 +0530

    change the service provider registration form

--- app/admin/service-providers/details/[id]/page.jsx
+++ app/admin/service-providers/details/[id]/page.jsx
@@ -3,7 +3,15 @@ import axios from "axios";
 import { useParams } from "next/navigation";
 import { useState, useEffect, useCallback } from "react";
 import { VscLoading } from "react-icons/vsc";
-import { Button, ListItem, ListItemSuffix } from "@material-tailwind/react";
+import {
+  Button,
+  ListItem,
+  ListItemSuffix,
+  Menu,
+  MenuHandler,
+  MenuItem,
+  MenuList,
+} from "@material-tailwind/react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { FaSlash, FaUserAltSlash } from "react-icons/fa";
 import Image from "next/image";
@@ -11,6 +19,8 @@ import { toast } from "sonner";
 import ServiceProviderLocation from "@/components/service-provider/ServiceProviderLocation";
 import ServicesList from "@/components/service-provider/ServiceList";
 import Link from "next/link";
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { PiBook } from "react-icons/pi";
 
 const ServiceProviderDetailsPage = () => {
   const { id } = useParams();
@@ -132,6 +142,41 @@ const ServiceProviderDetailsPage = () => {
         <h1 className="text-2xl font-bold text-indigo-500 font-lato text-center">
           Service Provider Details
         </h1>
+        <Menu placement="bottom-end">
+          <MenuHandler>
+            <Button color="indigo">Action</Button>
+          </MenuHandler>
+          <MenuList>
+            <MenuItem>
+              <Link
+                href={`/admin/bookings?search=${serviceProvider.name}`}
+                className="flex gap-3"
+              >
+                View Bookings
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link
+                href={`/admin/payments?search=${serviceProvider.name}`}
+                className="flex gap-3"
+              >
+                View Payout
+              </Link>
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                serviceProviderDeleting(serviceProvider);
+              }}
+            >
+              Delete Service Provider
+            </MenuItem>
+            <MenuItem
+              onClick={() => serviceProviderDeactivating(serviceProvider)}
+            >
+              <span>{serviceProvider.active ? "Deactivate" : "Activate"}</span>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </div>
       <div>
         {/* Dialog content */}
@@ -213,7 +258,18 @@ const ServiceProviderDetailsPage = () => {
         </div>
       </div>
       {/* Dialog Footer */}
-      <div className="flex items-center gap-2 justify-end p-0 py-5">
+      {/* <div className="flex items-center gap-2 justify-end p-0 py-5">
+        <Link href={`/admin/bookings?search=${serviceProvider.name}`}>
+          <Button
+            variant="gradient"
+            size="sm"
+            color="indigo"
+            className="flex items-center gap-1 rounded"
+          >
+            view bookings
+            <PiBook />
+          </Button>
+        </Link>
         <Link href={`/admin/payments?search=${serviceProvider.name}`}>
           <Button
             variant="gradient"
@@ -222,7 +278,7 @@ const ServiceProviderDetailsPage = () => {
             className="flex items-center gap-1 rounded"
           >
             view payout
-            <AiOutlineDelete />
+            <RiSecurePaymentLine />
           </Button>
         </Link>
         <Button
@@ -247,7 +303,7 @@ const ServiceProviderDetailsPage = () => {
           <span>{serviceProvider.active ? "Deactivate" : "Activate"}</span>
           <FaUserAltSlash />
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 };
--- components/admin/bookings/TableHeading.js
+++ components/admin/bookings/TableHeading.js
@@ -4,16 +4,24 @@ import React from "react";
 import { MdOutlineRefresh } from "react-icons/md";
 import { toast } from "sonner";
 
-const TableHeading = ({ setSearchQuery, setStatus, fetchBookings }) => {
+const TableHeading = ({
+  setSearchQuery,
+  setStatus,
+  fetchBookings,
+  searchQuery,
+}) => {
   return (
     <div className="bg-white p-4 rounded-lg border flex gap-2 justify-between flex-col md:flex-row">
-      <h1 className="text-2xl font-bold text-blue-gray-500 text-center md:text-left">All Bookings</h1>
+      <h1 className="text-2xl font-bold text-blue-gray-500 text-center md:text-left">
+        All Bookings
+      </h1>
       <div className="w-full md:w-2/3 lg:w-1/2 flex gap-4 flex-col lg:flex-row">
         <Input
           onChange={(e) => {
             setSearchQuery(e.target.value);
           }}
           label="Search By ID, Name, Phone number"
+          value={searchQuery}
           icon={<MagnifyingGlassIcon className="w-4 h-4" />}
         />
         <div className="w-full md:w-1/2 flex items-center gap-4">
